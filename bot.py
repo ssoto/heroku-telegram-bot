449ead4b88a35dd1958654f7aa4bdfbdce08b8fc
Sergio Soto Núñez <scots4ever@gmail.com>
2018-08-05 19:41:13 +0200

    Adds own bot logic

--- bot.py
+++ bot.py
@@ -1,20 +1,61 @@
+#! /usr/bin/python
 # -*- coding: utf-8 -*-
-import redis
-import os
-import telebot
-# import some_api_lib
-# import ...
 
-# Example of your code beginning
-#           Config vars
-token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
-#             ...
+import logging
 
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          InlineQueryHandler, )
 
-#       Your bot code below
-# bot = telebot.TeleBot(token)
-# some_api = some_api_lib.connect(some_api_token)
-#              ...
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+TOKEN = '585421530:AAEvq5r9Imkeuhcr95axCauIfENu_oMdkyU'
+
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
+
+greeting_message = ''' Hola soy el bot del proyecto Som Aigua
+
+Ahora mismo estoy programado para:
+* Procesar enlaces
+
+Para ello necesitas darte de alta como usuaria. Escribe a Sergio Soto para poder
+empezar a colaborar con nosotras.
+'''
+
+def start(bot, update):
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text=greeting_message)
+start_handler = CommandHandler('start', start)
+logging.info('Added start handler')
+dispatcher.add_handler(start_handler)
+
+def info_text(bot, update):
+    message = update.message.text
+
+    reply = '''
+    > {}
+    El texto ha sido guardado'''.format(message)
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text=reply
+    )
+
+info_handler = CommandHandler('info', info_text)
+dispatcher.add_handler(info_handler)
+logging.info('Added info handler')
+
+
+def unknown(bot, update):
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text="Perdona, pero no tengo configurado ese comando",
+    )
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+logging.info('Added unknown handler')
+
+
+updater.start_polling()
